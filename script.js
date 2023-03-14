62c456d42348df8480bd79931bf5e8d8491f5912
MD ARIF <arifahmad207@gmail.com>
2023-03-14 14:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function threeSum(arr, target) {
-// write your code here
-  
-}
+  arr.sort((a, b) => a - b); // sort the array in ascending order
 
-module.exports = threeSum;
+  let closestSum = Infinity;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum > target) {
+        right--;
+      } else if (currentSum < target) {
+        left++;
+      } else {
+        // if the sum is equal to the target, we have found the solution
+        return closestSum;
+      }
+    }
+  }
+
+  return closestSum;
+}
